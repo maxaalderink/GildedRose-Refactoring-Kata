8c66f8e3edd02b1726770ae34437775457124784
max_aalderink <maxaalderink@gmail.com>
2021-04-14 17:43:01 +0200

    refactored test to check if default update behaviour does not decrease quality bellow zero.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void updateDefault_QualityIsZero_QualityDoesNotDecreaseBellowZero() {
+        Item[] items = new Item[] { new Item("default", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals("default", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
     }
 
 }
