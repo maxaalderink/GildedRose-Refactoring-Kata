39ec1cb5e4d39c60b43b566eae05054225aba6bc
max_aalderink <maxaalderink@gmail.com>
2021-04-14 17:43:12 +0200

    added tests to check if default update behaviour decrease quality for situations where SellIn is above or below zero.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -16,4 +16,24 @@ class GildedRoseTest {
         assertEquals(0, app.items[0].quality);
     }
 
+    @Test
+    void updateDefault_SellInIsAboveZero_QualityDecrementsOne() {
+        Item[] items = new Item[] { new Item("default", 1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("default", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(9, app.items[0].quality);
+    }
+
+    @Test
+    void updateDefault_SellInIsZeroOrBellow_QualityDecrementsTwo() {
+        Item[] items = new Item[] { new Item("default", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("default", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
+    }
+
 }
